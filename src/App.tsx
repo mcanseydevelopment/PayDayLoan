5f560e2e854d9cd9a57e6ac233587d265a01c60a
Siddhesh <development@mcansey.com>
2025-03-01 17:53:55 +0530

    making changes

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import HowItWorks from './components/HowItWorks'
 // import ImageOnLeft from './components/ImageOnLeft'
 // import ImageOnRight from './components/ImageOnRight'
 // import ImportantDisclosures from './components/ImportantDisclosures'
-// import Navbar from './components/Navbar'
+import Navbar from './components/Navbar'
 // import ProductFeatures from './components/ProductFeatures'
 // import TestimonialCarousel from './components/testimonials-slider'
 function App() { 
